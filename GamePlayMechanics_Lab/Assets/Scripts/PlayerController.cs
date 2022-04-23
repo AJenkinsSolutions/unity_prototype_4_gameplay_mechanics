fa39113d8f4bbd2494e6b41af27713d3d9e356ad
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-04-22 22:38:18 -0500

    Add local EnemyRigidbody Variables, Add PowerUp Coroutines timer, Add PowerUp Indicator, Add PowerUpIndicator Logic, Add PowerUp and Enmey Interaction Logic, Add PowerIndicator SeetActive Logic

--- GamePlayMechanics_Lab/Assets/Scripts/PlayerController.cs
+++ GamePlayMechanics_Lab/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     // Reference to our Focal-Point CameraRig 
     private GameObject focalPoint;
-
     private Rigidbody playerRb;
+   
 
     public bool hasPowerUp = false;
+    private float powerUpStrength = 8f;
+    private float powerUpDuration = 8f;
 
-    //Speed Configs 
+    public GameObject powerUpIndicator;
+
     public float speed;
-    //Inputs
     private float verticalInput;
 
 
@@ -22,6 +24,7 @@ public class PlayerController : MonoBehaviour
     {
         //Initalize players rigidbody 
         playerRb = GetComponent<Rigidbody>();
+        
 
         // Intializes our reference to focalpoint script in cameraRig
         focalPoint = GameObject.Find("FocalPoint");
@@ -32,11 +35,12 @@ public class PlayerController : MonoBehaviour
     {
         //Axis Input
         verticalInput = Input.GetAxis("Vertical");
-
+        // Move in the direction of the focal point
         playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
-        
+        powerUpIndicator.transform.position = transform.position + new Vector3(0.0f, -0.5f, 0.0f);
 
     }
+
     //  Detect Collision with Power
     //  hasPowerUp == true
     //  Destory PowerUp gameObject
@@ -44,9 +48,45 @@ public class PlayerController : MonoBehaviour
     {
         if (other.CompareTag("PowerUp"))
         {
+            powerUpIndicator.SetActive(true);
             Debug.Log("power up");
             hasPowerUp = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDownRoutine());
+        }
+    }
+
+
+    IEnumerator PowerUpCountDownRoutine()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerUp = false;
+        powerUpIndicator.SetActive(false);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+
+            Debug.Log("Player Collided with " + collision.gameObject.name + "Player hasPowerUp:" + hasPowerUp);
+
+            // DUE to collision we can now tap into that collisions GameObject
+            // Giving us access to Its components
+            // Therefore giving us the ability to apply forces etc to its components
+            // In theroy we could access that gameObjects Script
+            // Giving us access to its variables 
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            // Vector Math: Direction opposite (away) the player postion
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            //ADDING FORCE: awayFromPlayer
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+
+
+
         }
     }
 }
--- GamePlayMechanics_Lab/Assets/Scripts/RotateCamera.cs
+++ GamePlayMechanics_Lab/Assets/Scripts/RotateCamera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    //Decalre our focal point rigid body 
     private Rigidbody focalPointRb;
 
     private float horizontalInput;
@@ -13,15 +14,17 @@ public class RotateCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Intalize our Focal Poiunt Camera Rig
         focalPointRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Axis horizontal input
         horizontalInput = Input.GetAxis("Horizontal");
     
-
+        //Rotate Camera rig on horizontal input
         transform.Rotate(0.0f, 90.0f * speed * Time.deltaTime * horizontalInput, 0.0f);
         
 
